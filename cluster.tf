4f161e66a7c2f261016f0d83fbc7ee8b75063e6c
Ederson Torresini <boidacarapreta@gmail.com>
2018-09-13 16:35:03 +0000

    3 nodes on Compute Engine

---
+++ cluster.tf
@@ -0,0 +1,135 @@
+// Variables
+variable "gce_ssh_user" {
+  default = "boidacarapreta"
+}
+
+variable "gce_ssh_pub_key_file" {
+  default = "id_rsa.pub"
+}
+
+// Provider
+provider "google" {
+  // https://console.cloud.google.com/apis/credentials/serviceaccountkey
+  credentials = "${file("gcp.json")}"
+  project     = "development-215518"
+  region      = "southamerica-east1"
+}
+
+// Network
+resource "google_compute_network" "network-0" {
+  name = "network-0"
+}
+
+resource "google_compute_subnetwork" "subnet-0" {
+  name          = "subnet-0"
+  ip_cidr_range = "10.0.0.0/24"
+  network       = "${google_compute_network.network-0.self_link}"
+}
+
+resource "google_compute_address" "address-0" {
+  name         = "address-0"
+  subnetwork   = "${google_compute_subnetwork.subnet-0.self_link}"
+  address_type = "INTERNAL"
+  address      = "10.0.0.100"
+}
+
+resource "google_compute_address" "address-1" {
+  name         = "address-1"
+  subnetwork   = "${google_compute_subnetwork.subnet-0.self_link}"
+  address_type = "INTERNAL"
+  address      = "10.0.0.101"
+}
+
+resource "google_compute_address" "address-2" {
+  name         = "address-2"
+  subnetwork   = "${google_compute_subnetwork.subnet-0.self_link}"
+  address_type = "INTERNAL"
+  address      = "10.0.0.102"
+}
+
+resource "google_compute_firewall" "firewall-0" {
+  name    = "firewall-0"
+  network = "${google_compute_network.network-0.name}"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "443"]
+  }
+}
+
+// Compute
+resource "google_compute_instance" "vm-0" {
+  name         = "vm-0"
+  machine_type = "n1-standard-1"
+  zone         = "southamerica-east1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    subnetwork    = "${google_compute_subnetwork.subnet-0.name}"
+    address       = "${google_compute_address.address-0.address}"
+    access_config = {}
+  }
+
+  metadata {
+    sshKeys = "${var.gce_ssh_user}:${file(var.gce_ssh_pub_key_file)}"
+  }
+
+  tags = ["k8s"]
+}
+
+resource "google_compute_instance" "vm-1" {
+  name         = "vm-1"
+  machine_type = "n1-standard-1"
+  zone         = "southamerica-east1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    subnetwork    = "${google_compute_subnetwork.subnet-0.name}"
+    address       = "${google_compute_address.address-1.address}"
+    access_config = {}
+  }
+
+  metadata {
+    sshKeys = "${var.gce_ssh_user}:${file(var.gce_ssh_pub_key_file)}"
+  }
+
+  tags = ["k8s"]
+}
+
+resource "google_compute_instance" "vm-2" {
+  name         = "vm-2"
+  machine_type = "n1-standard-1"
+  zone         = "southamerica-east1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    subnetwork    = "${google_compute_subnetwork.subnet-0.name}"
+    address       = "${google_compute_address.address-2.address}"
+    access_config = {}
+  }
+
+  metadata {
+    sshKeys = "${var.gce_ssh_user}:${file(var.gce_ssh_pub_key_file)}"
+  }
+
+  tags = ["k8s"]
+}
