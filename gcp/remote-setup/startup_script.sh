b663398e74c304d750b714124f41992601945687
Mat Muller <matuzalemmuller@gmail.com>
2018-09-23 03:15:31 -0300

    Add creation of script to give user permission to run rocker

--- gcp/remote-setup/startup_script.sh
+++ gcp/remote-setup/startup_script.sh
@@ -2,18 +2,18 @@
 
 USERNAME="matuzalemmuller"
 
-# Install docker
-sudo apt update
-wget -O docker.deb https://download.docker.com/linux/debian/dists/stretch/pool/stable/amd64/docker-ce_17.03.3~ce-0~debian-stretch_amd64.deb
-sudo dpkg -i docker.deb
-sudo apt -f -y install
-rm -rf docker*
-
-# Create file with command to add permission for user to run docker
+# Create script to install docker and give user permission to run it
 cd /home
 touch docker_setup.sh
 echo "#!/bin/bash" >> docker_setup.sh
+echo "cd $USERNAME"
+echo "sudo apt update" >> docker_setup.sh
+echo "wget -O docker.deb https://download.docker.com/linux/debian/dists/stretch/pool/stable/amd64/docker-ce_17.03.3~ce-0~debian-stretch_amd64.deb" >> docker_setup.sh
+echo "sudo dpkg -i docker.deb" >> docker_setup.sh
+echo "sudo apt -f -y install" >> docker_setup.sh
 echo "sudo usermod -aG docker $USERNAME" >> docker_setup.sh
+echo "rm -rf docker*" >> docker_setup.sh
+echo "rm ../docker_setup.sh" >> docker_setup.sh
 
 # Create file to delete monitor files from rook
 touch clear_rook_dataDir.sh
